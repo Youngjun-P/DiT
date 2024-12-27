f0d049ff1c34f318eaa301a93a7b3a87648884ee
Youngjun <youngjun@sogang.ac.kr>
2024-12-27 14:37:21 +0900

    DiT sampling by using RADiT

--- sample.py
+++ sample.py
@@ -65,7 +65,7 @@ def main(args):
     samples = vae.decode(samples / 0.18215).sample
 
     # Save and display images:
-    save_image(samples, "sample.png", nrow=4, normalize=True, value_range=(-1, 1))
+    save_image(samples, "../results/sample.png", nrow=4, normalize=True, value_range=(-1, 1))
 
 
 if __name__ == "__main__":
